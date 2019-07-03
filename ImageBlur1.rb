268f360ecde83f13ad3ebc2fd734966bbda2205e
Ashley <ashjohnson318@gmail.com>
2019-07-03 21:45:52 +0000

    Initial Imageblur1 commit

---
+++ ImageBlur1.rb
@@ -0,0 +1,21 @@
+class Image
+
+	def initialize (pictures)
+	 @pictures = pictures
+	end
+
+	def output_image
+	 @pictures.each do |array|
+	 puts array.join
+	end
+  end
+
+end
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+image.output_image
