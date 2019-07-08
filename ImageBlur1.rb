18403644dbef8b0338f87ab3e6a893b34e6fc17e
Ashley <ashjohnson318@gmail.com>
2019-07-08 20:27:53 +0000

    ImageBlur2

--- ImageBlur1.rb
+++ ImageBlur1.rb
@@ -10,7 +10,33 @@ class Image
 	end
   end
 
+  	def blur!(distance=1)
+  	  distance.times do
+  	 	 blur_coords!
+  	 	end
+	  end
+
+private 
+
+	def blur_coords!
+		blur_coords = []
+		@pictures.each_with_index do |array, i|
+			array.each_with_index do |x, array_i|
+				blur_coords << [i, array_i] if x == 1
+			end
+		end
+
+		blur_coords.each do |coord|
+			@pictures[coord[0]][coord[1] + 1] = 1 if coord[1] + 1 <= @pictures[coord[0]].length - 1
+			@pictures[coord[0]][coord[1] - 1] = 1 if coord[1] - 1 >= 0
+			@pictures[coord[0] + 1][coord[1]] = 1 if coord[0] + 1 <= @pictures.length - 1
+			@pictures[coord[0] - 1][coord[1]] = 1 if coord[0] - 1 >= 0
+		end
+	end
+
 end
+
+
 
 image = Image.new([
   [0, 0, 0, 0],
@@ -18,4 +44,5 @@ image = Image.new([
   [0, 0, 0, 1],
   [0, 0, 0, 0]
 ])
+image.blur!(1)
 image.output_image
