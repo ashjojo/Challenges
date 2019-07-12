e60c1b9fbcc15f307e81a3af4d0a1b394b3e6bbf
Ashley <ashjohnson318@gmail.com>
2019-07-12 16:36:39 +0000

    Fix Image Blur files

--- ImageBlur1.rb
+++ ImageBlur1.rb
@@ -1,42 +1,15 @@
 class Image
 
 	def initialize (pictures)
-	 @pictures = pictures
+	 	@pictures = pictures
 	end
 
 	def output_image
-	 @pictures.each do |array|
-	 puts array.join
-	end
-  end
-
-  	def blur!(distance=1)
-  	  distance.times do
-  	 	 blur_coords!
-  	 	end
-	  end
-
-private 
-
-	def blur_coords!
-		blur_coords = []
-		@pictures.each_with_index do |array, i|
-			array.each_with_index do |x, array_i|
-				blur_coords << [i, array_i] if x == 1
-			end
+	 	@pictures.each do |array|
+	 		puts array.join
 		end
-
-		blur_coords.each do |coord|
-			@pictures[coord[0]][coord[1] + 1] = 1 if coord[1] + 1 <= @pictures[coord[0]].length - 1
-			@pictures[coord[0]][coord[1] - 1] = 1 if coord[1] - 1 >= 0
-			@pictures[coord[0] + 1][coord[1]] = 1 if coord[0] + 1 <= @pictures.length - 1
-			@pictures[coord[0] - 1][coord[1]] = 1 if coord[0] - 1 >= 0
-		end
-	end
-
+  	end
 end
-
-
 
 image = Image.new([
   [0, 0, 0, 0],
@@ -44,5 +17,4 @@ image = Image.new([
   [0, 0, 0, 1],
   [0, 0, 0, 0]
 ])
-image.blur!(1)
 image.output_image
